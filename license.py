b6ffb697d15548f62442cece5142814dce4c625a
OBTTHT <95370738+OBTTHT@users.noreply.github.com>
2023-01-11 18:53:42 +0300

    Add files via upload

---
+++ license.py
@@ -0,0 +1,81 @@
+import datetime
+
+an = datetime.datetime.now()
+yıl = an.year
+
+name = str(input("Adınızı Ve Soyadınızı Girin: "))
+age = int(input("Yaşınızı Girin: "))
+LicenseType = str(input("Hangi Ehliyet Türünü Almak İstiyorsunuz: "))
+allLicenseType = ["DE","D","D1E","D1","CE","C","A","G","F","C1E","C1","BE","B","A2","B1","A1","M"]
+
+print("Yıl: " + str(yıl))
+print("Adınız: " + name)
+
+if (LicenseType == ("M") and age >= 16):
+    print("Ehliyet Sınıfı: " + LicenseType + " Yaşınız: " + str(age) + " Ehliyet Alabilirsiniz.")
+if (LicenseType == ("M") and age < 16):
+    print(LicenseType + " Ehliyet Türünü Almaya Yaşınız Yetmemektedir, " + str(16 - age) + " Yıl Sonra Alabilirsiniz.")
+if (LicenseType == ("A1") and age >= 16):
+    print("Ehliyet Sınıfı: " + LicenseType + " Yaşınız: " + str(age) + " Ehliyet Alabilirsiniz.")
+if (LicenseType == ("A1") and age < 16):
+    print(LicenseType + " Ehliyet Türünü Almaya Yaşınız Yetmemektedir, " + str(16 - age) + " Yıl Sonra Alabilirsiniz.")
+if (LicenseType == ("B1") and age >= 16):
+    print("Ehliyet Sınıfı: " + LicenseType + " Yaşınız: " + str(age) + " Ehliyet Alabilirsiniz.")
+if (LicenseType == ("B1") and age < 16):
+    print(LicenseType + " Ehliyet Türünü Almaya Yaşınız Yetmemektedir, " + str(16 - age) + " Yıl Sonra Alabilirsiniz.")
+if (LicenseType == ("A2") and age >= 18):
+    print("Ehliyet Sınıfı: " + LicenseType + " Yaşınız: " + str(age) + " Ehliyet Alabilirsiniz.")
+if (LicenseType == ("A2") and age < 18):
+    print(LicenseType + " Ehliyet Türünü Almaya Yaşınız Yetmemektedir, " + str(18 - age) + " Yıl Sonra Alabilirsiniz.")
+if (LicenseType == ("B") and age >= 18):
+    print("Ehliyet Sınıfı: " + LicenseType + " Yaşınız: " + str(age) + " Ehliyet Alabilirsiniz.")
+if (LicenseType == ("B") and age < 18):
+    print(LicenseType + " Ehliyet Türünü Almaya Yaşınız Yetmemektedir, " + str(18 - age) + " Yıl Sonra Alabilirsiniz.")
+if (LicenseType == ("BE") and age >= 18):
+    print("Ehliyet Sınıfı: " + LicenseType + " Yaşınız: " + str(age) + " Ehliyet Alabilirsiniz.")
+if (LicenseType == ("BE") and age < 18):
+    print(LicenseType + " Ehliyet Türünü Almaya Yaşınız Yetmemektedir, " + str(18 - age) + " Yıl Sonra Alabilirsiniz.")
+if (LicenseType == ("C1") and age >= 18):
+    print("Ehliyet Sınıfı: " + LicenseType + " Yaşınız: " + str(age) + " Ehliyet Alabilirsiniz.")
+if (LicenseType == ("C1") and age < 18):
+    print(LicenseType + " Ehliyet Türünü Almaya Yaşınız Yetmemektedir, " + str(18 - age) + " Yıl Sonra Alabilirsiniz.")
+if (LicenseType == ("C1E") and age >= 18):
+    print("Ehliyet Sınıfı: " + LicenseType + " Yaşınız: " + str(age) + " Ehliyet Alabilirsiniz.")
+if (LicenseType == ("C1E") and age < 18):
+    print(LicenseType + " Ehliyet Türünü Almaya Yaşınız Yetmemektedir, " + str(18 - age) + " Yıl Sonra Alabilirsiniz.")
+if (LicenseType == ("F") and age >= 18):
+    print("Ehliyet Sınıfı: " + LicenseType + " Yaşınız: " + str(age) + " Ehliyet Alabilirsiniz.")
+if (LicenseType == ("F") and age < 18):
+    print(LicenseType + " Ehliyet Türünü Almaya Yaşınız Yetmemektedir, " + str(18 - age) + " Yıl Sonra Alabilirsiniz.")
+if (LicenseType == ("G") and age >= 18):
+    print("Ehliyet Sınıfı: " + LicenseType + " Yaşınız: " + str(age) + " Ehliyet Alabilirsiniz.")
+if (LicenseType == ("G") and age < 18):
+    print(LicenseType + " Ehliyet Türünü Almaya Yaşınız Yetmemektedir, " + str(18 - age) + " Yıl Sonra Alabilirsiniz.")
+if (LicenseType == ("A") and age >= 20):
+    print("Ehliyet Sınıfı: " + LicenseType + " Yaşınız: " + str(age) + " Ehliyet Alabilirsiniz.")
+if (LicenseType == ("A") and age < 20):
+    print(LicenseType + " Ehliyet Türünü Almaya Yaşınız Yetmemektedir, " + str(20 - age) + " Yıl Sonra Alabilirsiniz.")
+if (LicenseType == ("C") and age >= 21):
+    print("Ehliyet Sınıfı: " + LicenseType + " Yaşınız: " + str(age) + " Ehliyet Alabilirsiniz.")
+if (LicenseType == ("C") and age < 21):
+    print(LicenseType + " Ehliyet Türünü Almaya Yaşınız Yetmemektedir, " + str(21 - age) + " Yıl Sonra Alabilirsiniz.")
+if (LicenseType == ("CE") and age >= 21):
+    print("Ehliyet Sınıfı: " + LicenseType + " Yaşınız: " + str(age) + " Ehliyet Alabilirsiniz.")
+if (LicenseType == ("CE") and age < 21):
+    print(LicenseType + " Ehliyet Türünü Almaya Yaşınız Yetmemektedir, " + str(21 - age) + " Yıl Sonra Alabilirsiniz.")
+if (LicenseType == ("D1") and age >= 21):
+    print("Ehliyet Sınıfı: " + LicenseType + " Yaşınız: " + str(age) + " Ehliyet Alabilirsiniz.")
+if (LicenseType == ("D1") and age < 21):
+    print(LicenseType + " Ehliyet Türünü Almaya Yaşınız Yetmemektedir, " + str(21 - age) + " Yıl Sonra Alabilirsiniz.")
+if (LicenseType == ("D1E") and age >= 21):
+    print("Ehliyet Sınıfı: " + LicenseType + " Yaşınız: " + str(age) + " Ehliyet Alabilirsiniz.")
+if (LicenseType == ("D1E") and age < 21):
+    print(LicenseType + " Ehliyet Türünü Almaya Yaşınız Yetmemektedir, " + str(21 - age) + " Yıl Sonra Alabilirsiniz.")
+if (LicenseType == ("D") and age >= 24):
+    print("Ehliyet Sınıfı: " + LicenseType + " Yaşınız: " + str(age) + " Ehliyet Alabilirsiniz.")
+if (LicenseType == ("D") and age < 24):
+    print(LicenseType + " Ehliyet Türünü Almaya Yaşınız Yetmemektedir, " + str(24 - age) + " Yıl Sonra Alabilirsiniz.")
+if (LicenseType == ("DE") and age >= 24):
+    print("Ehliyet Sınıfı: " + LicenseType + " Yaşınız: " + str(age) + " Ehliyet Alabilirsiniz.")
+if (LicenseType == ("DE") and age < 24):
+    print(LicenseType + " Ehliyet Türünü Almaya Yaşınız Yetmemektedir, " + str(24 - age) + " Yıl Sonra Alabilirsiniz.")
